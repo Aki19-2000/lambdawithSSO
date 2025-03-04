02c449e604511b1b76dc8a9536e0a44fe0e96b16
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-04 17:56:21 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -1,3 +1,15 @@
+# modules/api_gateway/main.tf
+
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "authorizer_name" {
+  description = "The name of the Cognito authorizer"
+  type        = string
+}
+
 resource "aws_api_gateway_rest_api" "api" {
   name        = "hello-world-api"
   description = "API Gateway for Hello World Lambda"
@@ -14,7 +26,7 @@ resource "aws_api_gateway_method" "get" {
   resource_id   = aws_api_gateway_resource.root.id
   http_method   = "GET"
   authorization = "COGNITO_USER_POOLS"
-  authorizer_id = var.authorizer_id
+  authorizer_id = var.authorizer_name  # Use the passed authorizer name here
 }
 
 resource "aws_api_gateway_integration" "lambda" {
@@ -23,7 +35,7 @@ resource "aws_api_gateway_integration" "lambda" {
   http_method             = aws_api_gateway_method.get.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.hello_world.arn}/invocations"
+  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_function_name}/invocations"
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
@@ -33,8 +45,3 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "prod"
 }
-
-output "api_gateway_url" {
-  value = "${aws_api_gateway_deployment.api_deployment.invoke_url}/hello"
-}
-
