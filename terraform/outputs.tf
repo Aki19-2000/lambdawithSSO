59d3c00eb07b09ba008bac963ed00208bd1affd6
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-04 17:25:00 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
+output "lambda_function_name" {
+  value = module.lambda.lambda_function_name
+}
+
+output "api_gateway_url" {
+  value = module.api_gateway.api_gateway_url
+}
+
+output "cognito_user_pool_id" {
+  value = module.cognito.user_pool_id
+}
 
