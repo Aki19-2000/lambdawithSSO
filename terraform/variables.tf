fdd147682c4b8f3a5cf040a1c1fed0c9834bb952
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-04 17:24:46 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,20 @@
 
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  default     = "HelloWorldFunction"
+}
+
+variable "user_pool_name" {
+  description = "Name of the Cognito User Pool"
+  default     = "HelloWorldUserPool"
+}
+
+variable "app_client_name" {
+  description = "Name of the Cognito App Client"
+  default     = "HelloWorldAppClient"
+}
