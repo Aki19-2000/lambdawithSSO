ec097d9eebb4118a0db2cc297d186be1b6ad0d23
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-04 17:25:18 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,1 +1,47 @@
+resource "aws_lambda_function" "hello_world" {
+  function_name = var.lambda_function_name
+  runtime       = "python3.9"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  
+  code {
+    zip_file = <<ZIP
+import json
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello World!')
+    }
+ZIP
+  }
+
+  environment {
+    variables = {
+      KEY = "value"
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect   = "Allow"
+        Sid      = ""
+      }
+    ]
+  })
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.hello_world.function_name
+}
 
