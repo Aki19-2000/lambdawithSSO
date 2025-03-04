8ae29f668c654811efff9bb21404c7e9dc42dd70
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-04 18:02:32 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,5 +1,12 @@
+# modules/lambda/main.tf
+
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
 resource "aws_lambda_function" "hello_world" {
-  function_name = var.lambda_function_name
+  function_name = var.lambda_function_name  # Use the passed name for the Lambda function
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
@@ -44,4 +51,3 @@ resource "aws_iam_role" "lambda_role" {
 output "lambda_function_name" {
   value = aws_lambda_function.hello_world.function_name
 }
-
