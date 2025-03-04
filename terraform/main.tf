65cd5b19ebf3d3a95ffcc6921e52887eea6b73e9
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-04 17:24:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,20 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+module "lambda" {
+  source = "./modules/lambda"
+  lambda_function_name = var.lambda_function_name
+}
+
+module "cognito" {
+  source = "./modules/cognito"
+  user_pool_name       = var.user_pool_name
+  app_client_name      = var.app_client_name
+}
+
+module "api_gateway" {
+  source               = "./modules/api_gateway"
+  lambda_function_name = module.lambda.lambda_function_name
+  authorizer_name      = module.cognito.authorizer_name
+}
