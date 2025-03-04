ca8d6db6572ce5f4aaab810b0186cdb9fb6b70af
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-04 17:59:52 +0530

    Update main.tf

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -1,3 +1,15 @@
+# modules/cognito/main.tf
+
+variable "user_pool_name" {
+  description = "The name of the Cognito User Pool"
+  type        = string
+}
+
+variable "app_client_name" {
+  description = "The name of the Cognito App Client"
+  type        = string
+}
+
 resource "aws_cognito_user_pool" "user_pool" {
   name = var.user_pool_name
 }
@@ -5,12 +17,12 @@ resource "aws_cognito_user_pool" "user_pool" {
 resource "aws_cognito_user_pool_client" "app_client" {
   name         = var.app_client_name
   user_pool_id = aws_cognito_user_pool.user_pool.id
-  generate_secret = false
+  generate_secret = false  # Set to true if you want a client secret
 }
 
 resource "aws_api_gateway_authorizer" "cognito_authorizer" {
   name                 = "CognitoAuthorizer"
-  rest_api_id          = aws_api_gateway_rest_api.api.id
+  rest_api_id          = var.rest_api_id
   authorizer_type      = "COGNITO_USER_POOLS"
   identity_source      = "method.request.header.Authorization"
   provider_arns        = [aws_cognito_user_pool.user_pool.arn]
@@ -20,8 +32,3 @@ resource "aws_api_gateway_authorizer" "cognito_authorizer" {
 output "authorizer_name" {
   value = aws_api_gateway_authorizer.cognito_authorizer.id
 }
-
-output "user_pool_id" {
-  value = aws_cognito_user_pool.user_pool.id
-}
-
