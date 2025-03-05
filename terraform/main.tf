7977e62027ecbcf291d0e003ac4b43fb7b5c0a63
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-05 13:51:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,28 @@
-# main.tf (Root Module)
-
 provider "aws" {
-  region = var.aws_region
+  region = var.aws_region  # Use the region variable
 }
 
 module "lambda" {
   source              = "./modules/lambda"
-  lambda_function_name = "HelloWorldFunction"  # Pass Lambda function name to the module
-}
-
-module "cognito" {
-  source          = "./modules/cognito"
-  user_pool_name  = "HelloWorldUserPool"
-  app_client_name = "HelloWorldAppClient"
+  lambda_function_name = "HelloWorldFunction"
 }
 
 module "api_gateway" {
-  source             = "./modules/api_gateway"
-  lambda_function_name = module.lambda.lambda_function_name  # Pass Lambda function name to API Gateway
-  authorizer_name      = module.cognito.authorizer_name      # Pass authorizer name to API Gateway
+  source               = "./modules/api_gateway"
+  lambda_function_name = module.lambda.lambda_function_name  # Correctly reference the output
+  authorizer_name      = var.authorizer_name                   # Pass authorizer name to API Gateway
+  aws_region           = var.aws_region                       # Pass region to API Gateway module
+  user_pool_arn        = "arn:aws:cognito-idp:us-east-1:510278866235:userpool/us-east-1_ma5Vf7L4Z"  # Reference the user pool ARN manually
+}
+
+variable "aws_region" {
+  description = "The AWS region"
+  type        = string
+  default     = "us-east-1"  # Set your default region here
+}
+
+variable "authorizer_name" {
+  description = "The name of the Cognito authorizer"
+  type        = string
+  default     = "MyCognitoAuthorizer"  # Set your default authorizer name
 }
