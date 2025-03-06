3b701a4ca648787dbba29c1f060d13954cee0cd5
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-06 10:13:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,17 +9,21 @@ module "iam" {
   lambda_function_name = var.lambda_function_name  # Pass Lambda function name
 }
 
-module "lambda_api" {
-  source = "./modules/lambda"
- 
-  lambda_role_arn     = module.iam.lambda_role_arn
-  image_uri           = var.image_uri
-  user_pool_id        = module.cognito.user_pool_id
-  user_pool_client_id = module.cognito.user_pool_client_id
-}
-
 module "cognito" {
   source = "./modules/cognito"
+}
+
+module "lambda_api" {
+  source               = "./modules/lambda"
+  region               = var.aws_region
+  lambda_function_name = var.lambda_function_name
+  iam_role_arn         = module.iam.lambda_role_arn
+  image_uri            = var.image_uri
+  environment          = "prd"
+  api_stage            = "prod"
+  account_id           = var.account_id
+  user_pool_id         = module.cognito.user_pool_id
+  user_pool_client_id  = module.cognito.user_pool_client_id
 }
 
 output "lambda_function_name" {
