8f95ebbd935f6af09594035e5848b8f5a02de777
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-06 09:39:27 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,10 +4,10 @@ variable "aws_region" {
   default     = "us-east-1"  # Default region, change it if needed
 }
 
-#variable "account_id" {
-  #description = "The AWS account ID"
-  #type        = string
-#}
+variable "account_id" {
+  description = "The AWS account ID"
+  type        = string
+}
 
 variable "lambda_function_name" {
   description = "Name of the Lambda function"
