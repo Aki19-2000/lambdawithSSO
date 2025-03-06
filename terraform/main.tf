4790ab99abc4fa33befcd53e2b75b206f6174b02
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-06 10:08:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,27 @@ provider "aws" {
 module "iam" {
   source               = "./modules/iam"
   aws_region           = var.aws_region       # Pass region to IAM module
-  account_id           = "510278866235"      # Pass account ID to IAM module
+  account_id           = var.account_id       # Pass account ID to IAM module
   lambda_function_name = var.lambda_function_name  # Pass Lambda function name
 }
 
 module "lambda_api" {
   source = "./modules/lambda"
  
-  lambda_role_arn     = module.iam_role.lambda_role_arn
-  image_name          = "510278866235.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest"
+  lambda_role_arn     = module.iam.lambda_role_arn
+  image_uri           = var.image_uri
   user_pool_id        = module.cognito.user_pool_id
   user_pool_client_id = module.cognito.user_pool_client_id
+}
+
+module "cognito" {
+  source = "./modules/cognito"
+}
+
+output "lambda_function_name" {
+  value = module.lambda_api.lambda_function_name
+}
+
+output "api_gateway_url" {
+  value = "https://${module.lambda_api.api_gateway_id}.execute-api.${var.aws_region}.amazonaws.com/prod"
 }
