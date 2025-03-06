4743eb5bd66c4fae003478d90f9f6d7007868e9d
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-06 10:10:08 +0530

    Delete terraform/outputs.tf

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lambda_function_name" {
-  value = module.lambda.lambda_function_name
-}
-
-output "api_gateway_url" {
-  value = "https://${module.api_gateway.rest_api_id}.execute-api.${var.aws_region}.amazonaws.com/prod"
-}
